2c4c5dc95e52ffd4cb876ef49996d9c524933ec4
Martinezs.J89 <Martinezs.J89@62b1ca8a-737a-36ee-2e1d-d25a5be0931e>
2011-11-24 03:30:06 +0000

    Clases proximamente interfaces

---
+++ primerProyecto-Sec4/src/Modelo/Partido.java
@@ -0,0 +1,48 @@
+package Modelo;
+import java.util.*;
+public class Partido {
+	private String codPartido;
+	public Date fechaJuego;
+	Equipo equipoLoc;
+	Equipo equipoVis;
+	Umpire umpire;
+	public int nroinnings;
+	public Partido() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Partido(String codPartido, Date fechaJuego, Equipo equipoLoc,
+			Equipo equipoVis, Umpire umpire, int nroinnings) {
+		super();
+		this.codPartido = codPartido;
+		this.fechaJuego = fechaJuego;
+		this.equipoLoc = equipoLoc;
+		this.equipoVis = equipoVis;
+		this.umpire = umpire;
+		this.nroinnings = nroinnings;
+	}
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+}
+	
+	
+	
+
+
+
+
+
+
+
+
